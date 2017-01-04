cf638e5374a8234e2c51b39ee7e79c1ebe9da1e9
Marco Selvi <marco.selvi@gmail.com>
2017-01-04 13:57:50 +0000

    Add removal script

---
+++ tools.py
@@ -0,0 +1,6 @@
+
+links_location = '/usr/local/bin'
+
+files_to_link = ['git-destroy', 'git-done', 'git-fullbranch', 'git-fullcheckout',
+                 'git-pullall', 'git-pushall', 'git-trackcheckout', 'git-update',
+                 'git-rebaseupdate', 'git-validate', 'git-prunelocal', 'git-updatefrom']
