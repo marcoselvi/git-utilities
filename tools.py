ed035477381209ec28b1df86b1e80fce43be3b8d
Marco Selvi <marco.selvi@gmail.com>
2017-10-06 16:05:08 +0100

    Unify two scripts into one

--- tools.py
+++ tools.py
@@ -1,6 +1,6 @@
 
 links_location = '/usr/local/bin'
 
-files_to_link = ['git-destroy', 'git-done', 'git-fullbranch', 'git-fullcheckout',
-                 'git-pullall', 'git-pushall', 'git-trackcheckout', 'git-update',
+files_to_link = ['git-destroy', 'git-done', 'git-fullbranch', 'git-create',
+                 'git-pullall', 'git-pushall', 'git-update',
                  'git-rebaseupdate', 'git-validate', 'git-prunelocal', 'git-updatefrom']
