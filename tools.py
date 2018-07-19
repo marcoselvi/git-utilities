f6eba76497108fbe41ddaa2cb9dceb0bdd49b426
Marco Selvi <marco.selvi@gmail.com>
2018-07-19 11:52:49 +0100

    Improve installation script

--- tools.py
+++ tools.py
@@ -1,6 +1,3 @@
-
-links_location = '/usr/local/bin'
-
 files_to_link = ['git-destroy', 'git-done', 'git-fullbranch', 'git-create',
                  'git-pullall', 'git-pushall', 'git-update',
                  'git-rebaseupdate', 'git-validate', 'git-prunelocal', 'git-updatefrom']
