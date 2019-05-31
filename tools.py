9d117761d2f929df321f4b6dad6ebac069ecd2e5
Marco Selvi <marco.selvi@gmail.com>
2019-05-31 18:26:31 +0100

    Improve install script

--- tools.py
+++
@@ -1,3 +0,0 @@
-files_to_link = ['git-destroy', 'git-done', 'git-fullbranch', 'git-create',
-                 'git-pullall', 'git-pushall', 'git-update',
-                 'git-rebaseupdate', 'git-validate', 'git-prunelocal', 'git-updatefrom']
